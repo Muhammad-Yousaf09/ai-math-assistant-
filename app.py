a2fc85c1fb0e745fef579a81a3692acd5f13b8a0
Muhammad Yousaf <150243472+Muhammad-Yousaf09@users.noreply.github.com>
2025-07-20 12:17:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,102 +1,107 @@
 import streamlit as st
 from langchain_groq import ChatGroq
-from langchain.chains import LLMChain,LLMMathChain
+from langchain.chains import LLMMathChain, LLMChain
 from langchain.prompts import PromptTemplate
 from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.agents.agent_types import AgentType
-from langchain.agents import Tool,initialize_agent
+from langchain.agents import Tool, initialize_agent
 from langchain.callbacks import StreamlitCallbackHandler
 
+## Set upi the Stramlit app
+st.set_page_config(page_title="Text To MAth Problem Solver And Data Serach Assistant",page_icon="🧮")
+st.title("Text To Math Problem Solver Uing Google Gemma 2")
 
-## set up the streamlit app
-st.set_page_config(page_title="Text To Math Problems Solver Data Search Assistant", page_icon=":robot:")
-st.title("Text To Math Problems Solver Data Search Assistant")
+groq_api_key=st.sidebar.text_input(label="Groq API Key",type="password")
 
-# Initialize the Groq model
-groq_api_key=st.sidebar.text_input("Enter your Groq API Key", type="password")
+
 if not groq_api_key:
-    st.warning("Please enter your Groq API Key to continue.")
-else:
-    st.success("Groq model initialized successfully!")
+    st.info("Please add your Groq APPI key to continue")
+    st.stop()
 
-# initialize the language model
-llm = ChatGroq(model="gemma2-9b-it", groq_api_key=groq_api_key)
+llm=ChatGroq(model="Gemma2-9b-It",groq_api_key=groq_api_key)
 
 
-## initializing the tools
-wikipedia = WikipediaAPIWrapper()
-wikipedia_tool = Tool(
-        name="Wikipedia",
-        func=wikipedia.run,
-        description="Searches Wikipedia for information.",
-        return_direct=True
-    )
+## Initializing the tools
+wikipedia_wrapper=WikipediaAPIWrapper()
+wikipedia_tool=Tool(
+    name="Wikipedia",
+    func=wikipedia_wrapper.run,
+    description="A tool for searching the Internet to find the vatious information on the topics mentioned"
 
-## initializing the Math Tool
-math_chain=LLMMathChain.from_llm(
-    llm=llm)
+)
+
+## Initializa the MAth tool
+
+math_chain=LLMMathChain.from_llm(llm=llm)
 calculator=Tool(
     name="Calculator",
     func=math_chain.run,
-    description="Useful for solving math problems.",
-    return_direct=True
-    )
-prompt="""You are a math problem solver. and mathematical question. logically arrive at the solution and display it point wise for the question below Please solve the following problem:
-Question: {question}
+    description="A tools for answering math related questions. Only input mathematical expression need to bed provided"
+)
+
+prompt="""
+Your a agent tasked for solving users mathemtical question. Logically arrive at the solution and provide a detailed explanation
+and display it point wise for the question below
+Question:{question}
 Answer:
 """
-prompt_template = PromptTemplate(
+
+prompt_template=PromptTemplate(
     input_variables=["question"],
     template=prompt
 )
 
+## Combine all the tools into chain
+chain=LLMChain(llm=llm,prompt=prompt_template)
 
-## combine all the tools into chain
-LLMChain = LLMChain(
+reasoning_tool=Tool(
+    name="Reasoning tool",
+    func=chain.run,
+    description="A tool for answering logic-based and reasoning questions."
+)
+
+## initialize the agents
+
+assistant_agent=initialize_agent(
+    tools=[wikipedia_tool,calculator,reasoning_tool],
     llm=llm,
-    prompt=prompt_template
-)
-
-## Adding reasoning tool
-reasoning_tool = Tool(
-    name="Reasoning",
-    func=LLMChain.run,
-    description="Useful for reasoning about math problems.",
-    return_direct=True
-)
-
-## initializing the agent
-assistant_agent = initialize_agent(
-    tools=[wikipedia_tool, calculator, reasoning_tool],
     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-    llm=llm,
-    verbose=True,
+    verbose=False,
     handle_parsing_errors=True
 )
 
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        {"role": "assistant", "content": "Hello, I need help with a math problem."}
+    st.session_state["messages"]=[
+        {"role":"assistant","content":"Hi, I'm a MAth chatbot who can answer all your maths questions"}
     ]
 
 for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+    st.chat_message(msg["role"]).write(msg['content'])
 
-
-# Lets start the interaction
-question = st.text_area("Enter your question ","What is the sum of 2 and 3?",)
+## LEts start the interaction
+question=st.text_area("Enter youe question:","I have 5 bananas and 7 grapes. I eat 2 bananas and give away 3 grapes. Then I buy a dozen apples and 2 packs of blueberries. Each pack of blueberries contains 25 berries. How many total pieces of fruit do I have at the end?")
 
 if st.button("find my answer"):
     if question:
-        with st.spinner("Generating response..."):
-            st.session_state.messages.append({"role": "user", "content": question})
+        with st.spinner("Generate response.."):
+            st.session_state.messages.append({"role":"user","content":question})
             st.chat_message("user").write(question)
 
-            st_cb= StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
-            response=assistant_agent.run(st.session_state.messages, callbacks=[st_cb])
-
-            st.session_state.messages.append({"role": "assistant", "content": response})
+            st_cb=StreamlitCallbackHandler(st.container(),expand_new_thoughts=False)
+            response=assistant_agent.run(st.session_state.messages,callbacks=[st_cb]
+                                         )
+            st.session_state.messages.append({'role':'assistant',"content":response})
             st.write('### Response:')
             st.success(response)
+
     else:
-        st.warning("Please enter a question to get an answer.")
+        st.warning("Please enter the question")
+
+
+
+
+
+
+
+
+
